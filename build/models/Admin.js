20b2fb0d4bce1a5eb2bc397552fa3dda931ffc9e
eladibyoussef <eladibyoussef230@gmail.com>
2024-03-21 14:45:19 +0000

    set up the passport jwt and authorization middlwares

--- build/models/Admin.js
+++ build/models/Admin.js
@@ -22,15 +22,52 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importStar(require("mongoose"));
+const bcrypt_1 = __importDefault(require("bcrypt"));
 const adminSchema = new mongoose_1.Schema({
-    id: { type: String, required: true },
+    ref: { type: String, required: true },
     username: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
     phoneNumber: { type: String, required: true },
     address: { type: String, required: true },
     permissions: [{ type: String, required: true }]
+});
+adminSchema.pre('save', function (next) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (!this.isModified('password')) {
+            return next();
+        }
+        try {
+            const salt = yield bcrypt_1.default.genSalt(10);
+            const hashedPassword = yield bcrypt_1.default.hash(this.password, salt);
+            this.password = hashedPassword;
+            next();
+        }
+        catch (error) {
+            console.log(error);
+            next(error);
+        }
+    });
+});
+adminSchema.set('toJSON', {
+    transform: function (doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+    }
 });
 exports.default = mongoose_1.default.model('Admin', adminSchema);
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import { Request, Response } from 'express';
+import adminRouter from './routes/adminRouter';
 // import { Application, urlencoded, json } from 'express';
-
 import db from './config/database';
 import * as dotenv from 'dotenv';
+import { authentication  } from './config/jwtPassport';
+import { checkAuthorization } from './middlewares/authorization';
+
 
 dotenv.config();
 
@@ -17,6 +21,16 @@ db.once('open', () => {
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+
+
+app.get('/superProtect',checkAuthorization('superAdmin'),authentication,(req:Request,res:Response)=>{  
+  res.status(200).json({msg:'you are connected'})
+})
+
+
+
+app.use('/admin', adminRouter)
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
