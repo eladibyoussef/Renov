598187601659fff3b320d025ca1e9764137cc43c
PeachyKhawla <chehmanik@gmail.com>
2024-03-22 22:35:02 +0000

    created product routes

--- server/src/controllers/productControllers.ts
+++ server/src/controllers/productControllers.ts
@@ -105,11 +105,44 @@ const searchProductsByPrice = async (req: Request, res: Response): Promise<void>
   }
 };
 
+
+// Search for product by any keyword provided by the user 
+
+const searchForProducts = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const queryParameter: string = (req.query.searchFor as string).trim();
+        const products: productDocument[] = await Product.find({
+            $or: [
+                { name: { $regex: queryParameter, $options: 'i' } },
+                { description: { $regex: queryParameter, $options: 'i' } },
+                { category: { $regex: queryParameter, $options: 'i' } },
+                { price: { $regex: queryParameter, $options: 'i' } },
+                { availability: { $regex: queryParameter, $options: 'i' } },
+                { rentable: { $regex: queryParameter, $options: 'i' } },
+                { photos: { $regex: queryParameter, $options: 'i' } },
+                { seller: { $regex: queryParameter, $options: 'i' } },
+                { deliveryFees: { $regex: queryParameter, $options: 'i' } }
+            ]
+        });
+
+        if (!products || products.length === 0) {
+            res.status(404).json({ msg: "Sorry, we don't have a product that matches the criteria" });
+        } else {
+            res.status(200).json({ products });
+        }
+    } catch (error: any) {
+        console.error('Error:', error.message);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+
 export {
     createProduct,
     getAllProducts,
     getProductById,
     updateProduct,
     deleteProduct,
-    searchProductsByPrice
+    searchProductsByPrice,
+    searchForProducts
   };
