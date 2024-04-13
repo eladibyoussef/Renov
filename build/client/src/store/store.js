e291f7e225973591a4a437dff86773474c52310d
eladibyoussef <eladibyoussef230@gmail.com>
2024-04-13 17:07:32 +0000

    testing render deploy

--- build/client/src/store/store.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.store = void 0;
-const toolkit_1 = require("@reduxjs/toolkit");
-exports.store = (0, toolkit_1.configureStore)({
-    reducer: {},
-});
