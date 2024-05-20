3496c302662b5a79ab84eb7c3de100ad2f8e89e5
eladibyoussef <eladibyoussef230@gmail.com>
2024-05-20 11:49:35 +0100

    setting up the admin auth

--- client/src/components/BackOfficeSideBar.tsx
+++ client/src/components/BackOfficeSideBar.tsx
@@ -7,6 +7,7 @@ import { TbShoppingCartSearch } from "react-icons/tb";
 import { IoChatbubblesOutline , IoSettingsOutline} from "react-icons/io5";
 import { CiLogout } from "react-icons/ci";
 import React ,  { useState  } from 'react';
+import { logout } from '@/Utils/login';
 
 const linkClass =
 	'flex items-center gap-2 font-Outfit text-customGrey font-normal  px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base'
@@ -42,7 +43,7 @@ function BackOfficeSideBar() {
       </div>
       <div>
       <Link to={'/backoffice/help'} className={linkClass}> <span><BiHelpCircle /></span> Help & infprmation</Link>
-      <Link to={'/backoffice/logout'} className={linkClass}> <span><CiLogout /></span> Logout</Link>
+      <Link to={'/admin'} className={linkClass} onClick={ ()=>{logout()}}> <span><CiLogout /></span> Logout</Link>
 
       </div>
     </div>
